e9d85bd8907c792c7d1ead20e5cf66bf10981ade
XWerer <dadomw2@gmail.com>
2018-03-30 10:35:18 +0200

    Initial commit

---
+++ src/main/java/it/unipd/dei/bdc1718/G08HM2.java
@@ -0,0 +1,26 @@
+package it.unipd.dei.bdc1718;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class G08HM2 {
+
+  public static void main(String[] args) throws FileNotFoundException {
+    if (args.length == 0){
+      throw new IllegalArgumentException("Expecting the file name on the command line");
+    }
+
+    /*
+    * HOMEWORK 2
+    * Raga non c'è scritto ancora niente,
+    * Mi raccomando quando fate gli update scrivete cosa fate e commentate anche il codice
+    * Fate sempre prima l'update del progetto e poi iniziate a scrivere
+    * */
+  }
+}
