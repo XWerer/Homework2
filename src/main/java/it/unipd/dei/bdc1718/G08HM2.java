552ebc49d9390451704abcef4d28497080e0b77e
XWerer <dadomw2@gmail.com>
2018-04-05 14:34:13 +0200

    Sistemato la cosa delle k parole piu frequenti

--- src/main/java/it/unipd/dei/bdc1718/G08HM2.java
+++ src/main/java/it/unipd/dei/bdc1718/G08HM2.java
@@ -4,18 +4,28 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Serializable;
 import scala.Tuple2;
-import scala.Tuple3;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class G08HM2 {
 
   //the number of words in text-sample.txt
   private static final long N = 3503570L;
+
+  //class for implement the compare method
+  public static class Tuple2Comparator implements Serializable, Comparator<Tuple2<String, Long>> {
+
+      //comparator for mathod top
+      public int compare(Tuple2<String, Long> a, Tuple2<String, Long> b) {
+          if (a._2() < b._2()) return -1;
+          else if (a._2() > b._2()) return 1;
+          return 0;
+      }
+  }
 
   /*
    * HOMEWORK 2
@@ -27,7 +37,7 @@ public class G08HM2 {
     if (args.length == 0)
       throw new IllegalArgumentException("Expecting the file name on the command line");
 
-    //Some variables
+    //Variables for measure the time
     long start, end;
 
     //Creation of the Spark Configuration
@@ -66,7 +76,7 @@ public class G08HM2 {
     end = System.currentTimeMillis();
     System.out.println("Elapsed time of word count 0: " + (end - start) + " ms");
 
-    //Start time
+    //Start time 1
     start = System.currentTimeMillis();
 
     //Word Count 1
@@ -91,17 +101,11 @@ public class G08HM2 {
               return sum;
             });
 
-    /* For visualizing the results
-    wordcount1.foreach(data -> {
-      System.out.println(data._1 + " - " + data._2);
-    });
-    */
-
-    //End time
+    //End time 1
     end = System.currentTimeMillis();
     System.out.println("Elapsed time of word count 1: " + (end - start) + " ms");
 
-    //Start time
+    //Start time 2
     start = System.currentTimeMillis();
 
     //Word Count 2
@@ -132,28 +136,44 @@ public class G08HM2 {
                 return pairs.iterator();
             }).reduceByKey((x,y) -> x+y);
 
-    //End time
+    //End time 2
     end = System.currentTimeMillis();
     System.out.println("Elapsed time of word count 2: " + (end - start) + " ms");
 
-    //Start time
+    //Start time 3
     start = System.currentTimeMillis();
 
     //Word Count 3
 
-    //End time
+    //End time 3
     end = System.currentTimeMillis();
     System.out.println("Elapsed time of word count 3: " + (end - start) + " ms");
 
+    //Some print for verifying the correctness of the code
     System.out.println("0. The number of distinct words is " + wordcounts.count());
     System.out.println("1. The number of distinct words is " + wordcounts1.count());
     System.out.println("2. The number of distinct words is " + wordcounts2.count());
     //System.out.println("3. The number of distinct words is " + wordcounts3.count());
 
-    //Da aggiungere la cosa delle k parole!
-    JavaRDD<Tuple2<Long, String>> x = wordcounts.map((pair) -> {
-        return new Tuple2(pair._2(), pair._1());
-    });
+    //Part of the k most frequent words
+    System.out.println("How much words?");
+    Scanner in  = new Scanner(System.in);
+    int k = in.nextInt();
+
+    List<Tuple2<String, Long>> k_words = wordcounts.top(k, new Tuple2Comparator());
+    for (Tuple2<String, Long> word : k_words) {
+        System.out.println("Case 0: " + word._1() + " - " + word._2());
+    }
+
+    List<Tuple2<String, Long>> k_words1 = wordcounts.top(k, new Tuple2Comparator());
+    for (Tuple2<String, Long> word : k_words1) {
+        System.out.println("Case 1: " + word._1() + " - " + word._2());
+    }
+
+    List<Tuple2<String, Long>> k_words2 = wordcounts.top(k, new Tuple2Comparator());
+    for (Tuple2<String, Long> word : k_words2) {
+        System.out.println("Case 2: " + word._1() + " - " + word._2());
+    }
 
     //Stop the end of the program for seeing the web interface
     System.out.println("Press enter to finish");
